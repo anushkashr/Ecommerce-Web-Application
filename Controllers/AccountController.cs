1b1395a73542c752fe0836f1626086552d95c1cf
anushkashr <99557370+anushkashr@users.noreply.github.com>
2023-08-06 21:08:21 -0500

    Add project files.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,47 @@
+﻿using Ecommerce_WebApp.ViewModel;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Ecommerce_WebApp.Controllers
+{
+	public class AccountController : Controller
+	{
+		private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(SignInManager<IdentityUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+        public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Login() 
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Login(LoginViewModel model)
+		{
+			if (ModelState.IsValid) 
+			{
+				var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, false, false);
+
+				if(result.Succeeded)
+				{
+					return RedirectToAction("Index", "Home");
+				}
+				ModelState.AddModelError("", "Invalid email or password");
+			}
+			return View(model);
+		}
+		public IActionResult Register()
+		{
+			return View();
+		}
+
+	}
+}
---
+++ ViewModel/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ecommerce_WebApp.ViewModel
+{
+	public class LoginViewModel
+	{
+		[EmailAddress]
+		[Required]
+		public string? Email { get; set; }
+		[Required]
+		[DataType(DataType.Password)]
+		public string? Password { get; set; }
+
+		public string RememberMe { get; set; }
+	}
+}
